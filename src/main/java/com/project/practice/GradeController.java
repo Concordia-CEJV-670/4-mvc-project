a9ec6f259c080bdd2243a99a3207ee5c30ca23f4
PranavBhatia <bhatiapranav1996@gmail.com>
2024-02-06 17:45:32 -0500

    Loops with bug

--- src/main/java/com/project/practice/GradeController.java
+++ src/main/java/com/project/practice/GradeController.java
@@ -1,5 +1,8 @@
 package com.project.practice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GradeController {
 
+    List<Grade> studentGrades = new ArrayList<>();
+
     @GetMapping("/grades")
     public String getGrades(Model model) {
-        Grade grade = new Grade("Meli", "B+", "Math");
-        model.addAttribute("grade", grade);
+        studentGrades.add(new Grade("Meli", "B+", "Math"));
+        studentGrades.add(new Grade("Jack", "C-", "Physics"));
+        studentGrades.add(new Grade("Nina", "A", "Biology"));
+        model.addAttribute("grades", studentGrades);
         return "grades";
     }
 }
