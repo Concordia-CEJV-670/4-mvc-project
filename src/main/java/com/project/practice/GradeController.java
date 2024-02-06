7d5e91fcd7641be0f74ddecaa82cb8bad43b368e
PranavBhatia <bhatiapranav1996@gmail.com>
2024-02-06 17:49:08 -0500

    Loops bugfix

--- src/main/java/com/project/practice/GradeController.java
+++ src/main/java/com/project/practice/GradeController.java
@@ -1,6 +1,6 @@
 package com.project.practice;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GradeController {
 
-    List<Grade> studentGrades = new ArrayList<>();
+    List<Grade> studentGrades = Arrays.asList(
+            new Grade("Meli", "B+", "Math"),
+            new Grade("Jack", "C-", "Physics"),
+            new Grade("Nina", "A", "Biology"));
 
     @GetMapping("/grades")
     public String getGrades(Model model) {
-        studentGrades.add(new Grade("Meli", "B+", "Math"));
-        studentGrades.add(new Grade("Jack", "C-", "Physics"));
-        studentGrades.add(new Grade("Nina", "A", "Biology"));
         model.addAttribute("grades", studentGrades);
         return "grades";
     }
